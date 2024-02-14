9736fbed80a6dc4a06208a27e15d28ab2330a1ae
Nikita Kolesnikov <nk_exp@mail.ru>
2024-02-14 17:45:21 +0300

    add memo to input component

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,29 +1,27 @@
 import { classNames } from "helpers";
 import style from "./Input.module.scss";
-import { InputHTMLAttributes, useEffect, useRef } from "react";
+import { InputHTMLAttributes, memo, useEffect, useRef } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   autoFocus?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({
-  className,
-  autoFocus,
-  ...otherProps
-}) => {
-  const inputRef = useRef<HTMLInputElement>(null);
+const Input: React.FC<InputProps> = memo(
+  ({ className, autoFocus, ...otherProps }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    if (autoFocus && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [autoFocus]);
+    useEffect(() => {
+      if (autoFocus && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, [autoFocus]);
 
-  return (
-    <div className={classNames(style.Input, {}, [className])}>
-      <input ref={inputRef} {...otherProps} />
-    </div>
-  );
-};
+    return (
+      <div className={classNames(style.Input, {}, [className])}>
+        <input ref={inputRef} {...otherProps} />
+      </div>
+    );
+  }
+);
 export default Input;
